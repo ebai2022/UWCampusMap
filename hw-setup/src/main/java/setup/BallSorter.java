85d5201185eba07d1edc755044a5feb722a51370
Ethan <ebai2022@cs.washington.edu>
2023-01-19 10:35:58 -0800

    All tests passed

--- hw-setup/src/main/java/setup/BallSorter.java
+++ hw-setup/src/main/java/setup/BallSorter.java
@@ -2,7 +2,19 @@ package setup;
 
 import java.util.Comparator;
 
+/**
+ * Compares two balls based on their volumes
+ */
 public class BallSorter implements Comparator<Ball>  {
+    /**
+     * Compares two balls to one another
+     *
+     * @param n Ball to be compared to m Ball
+     * @return -1 if the volume of n < volume m, 0 if the volumes are equal,
+     * and 1 if volume n > volume m
+     * n != null and m != null
+     */
+    @Override
     public int compare(Ball n, Ball m){
         double diff = n.getVolume() - m.getVolume();
         if (diff < 0){
