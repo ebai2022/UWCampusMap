a1ce14337d4b46fe984cee18a7627b442e566855
Ethan <ebai2022@cs.washington.edu>
2023-03-02 14:36:36 -0800

    rough backend finished!

--- hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
+++ hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
@@ -12,6 +12,15 @@
 package campuspaths;
 
 import campuspaths.utils.CORSFilter;
+import com.google.gson.Gson;
+import pathfinder.CampusMap;
+import pathfinder.datastructures.Path;
+import pathfinder.datastructures.Point;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import java.util.*;
 
 public class SparkServer {
 
@@ -24,6 +33,32 @@ public class SparkServer {
         // You should leave these two lines at the very beginning of main().
 
         // TODO: Create all the Spark Java routes you need here.
+        CampusMap map = new CampusMap("campus_buildings.csv", "campus_paths.csv");
+        Spark.get("/buildingNames", new Route(){
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                List<String> buildingShortNames = new ArrayList<>(map.buildingNames().keySet());
+                Gson gson = new Gson();
+                String jsonResponse = gson.toJson(buildingShortNames);
+                return jsonResponse;
+            }
+        });
+        Spark.get("/findPath", new Route(){
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                // don't need error handling because we will have a dropdown box and exactly what input we are giving
+                //http://localhost:4567/findPath?start=CSE&end=KNE calls findPath with start building being CSE and end being KNE
+                String startBuilding = request.queryParams("start");
+                String endBuilding = request.queryParams("end");
+                Path<Point> shortestPath = map.findShortestPath(startBuilding, endBuilding);
+                //if (startBuilding == null || endBuilding == null || !map.shortNameExists(startBuilding) || !map.shortNameExists(endBuilding)){
+                //    Spark.halt(400, "hwhooooops");
+                //}
+                Gson gson = new Gson();
+                String jsonResponse = gson.toJson(shortestPath);
+                return jsonResponse;
+            }
+        });
     }
 
 }
